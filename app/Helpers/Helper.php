c3b45c24fabec39894078c7c3cc09a72c705477b
Holong M. Situmorang <holongm.situmorang@gmail.com>
2021-12-28 10:43:42 +0700

    update check tanggal

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -95,3 +95,46 @@
 
         return $datajamlembur;
     }
+
+    function getNamaHariIni()
+    {
+
+        $hari = date ("D");
+
+        switch($hari){
+            case 'Sun':
+                $hari_ini = "Minggu";
+            break;
+
+            case 'Mon':         
+                $hari_ini = "Senin";
+            break;
+
+            case 'Tue':
+                $hari_ini = "Selasa";
+            break;
+
+            case 'Wed':
+                $hari_ini = "Rabu";
+            break;
+
+            case 'Thu':
+                $hari_ini = "Kamis";
+            break;
+
+            case 'Fri':
+                $hari_ini = "Jumat";
+            break;
+
+            case 'Sat':
+                $hari_ini = "Sabtu";
+            break;
+            
+            default:
+                $hari_ini = "Tidak di ketahui";     
+            break;
+        }
+
+        return $hari_ini;
+    }
+?>
--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -33,6 +33,11 @@ class APIController extends Controller
 
             $tanggal = date("Y-m-d");
 
+            $hari_ini=getNamaHariIni();
+            $tglHari_ini = date("d-m-Y");
+
+            $result["tanggal"] = $hari_ini.", ".$tglHari_ini;
+
             $absen = DB::select('select username, Cast(waktu_login as Date) as "tanggal", MIN(waktu_login) as "waktu_masuk", MAX(waktu_login) as "waktu_pulang" from td_login_history where username = "'.$request->username.'" and Date(waktu_login)="'.$tanggal.'" group by username, Cast(waktu_login as Date)');
 
             $result["jammasuk"] = "-";
@@ -72,6 +77,11 @@ class APIController extends Controller
 
         $result["jammasuk"] = "-";
         $result["jampulang"] = "-";
+
+        $hari_ini=getNamaHariIni();
+        $tglHari_ini = date("d-m-Y");
+
+        $result["tanggal"] = $hari_ini.", ".$tglHari_ini;
 
         if(!empty($absen))
         {
