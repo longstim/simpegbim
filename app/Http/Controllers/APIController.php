67ab8868e203c33604851797b3596330d34f8306
Holong M. Situmorang <holongm.situmorang@gmail.com>
2021-12-29 16:02:01 +0700

    iniate data update

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -108,4 +108,41 @@ class APIController extends Controller
            echo json_encode($result);
         }
     }
+
+    function getDataAwal(Request $request) 
+    {
+        setlocale(LC_ALL, 'IND');
+
+        $tanggal = date("Y-m-d");
+
+        $absen = DB::select('select username, Cast(waktu_login as Date) as "tanggal", MIN(waktu_login) as "waktu_masuk", MAX(waktu_login) as "waktu_pulang" from td_login_history where username = "'.$request->username.'" and Date(waktu_login)="'.$tanggal.'" group by username, Cast(waktu_login as Date)');
+
+        $result["jammasuk"] = "-";
+        $result["jampulang"] = "-";
+
+        $hari_ini=getNamaHariIni();
+        $tglHari_ini = date("d-m-Y");
+
+        $result["tanggal"] = $hari_ini.", ".$tglHari_ini;
+
+        if(!empty($absen))
+        {
+            $result["success"] = "1";
+            $result["message"] = "success";
+
+            $timestampmasuk = strtotime($absen[0]->waktu_masuk);
+            $timestamppulang = strtotime($absen[0]->waktu_pulang);
+
+            $result["jammasuk"] = date("H:i", $timestampmasuk);
+            $result["jampulang"] = date("H:i", $timestamppulang);
+
+            echo json_encode($result);
+        }
+        else 
+        {
+           $result["success"] = "0";
+           $result["message"] = "error";
+           echo json_encode($result);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/loginAndroid', 'APIController@loginAndroid');
 
 Route::post('/checkIN', 'APIController@checkIN');
+
+Route::post('/getDataAwal', 'APIController@getDataAwal');
